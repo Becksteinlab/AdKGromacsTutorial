636bebd0fb556a7e594bfecfbd9951773182caf5
Sean Seyler <slseyler@asu.edu>
2017-03-28 17:15:23 -0700

    Just removed all the old stuff since it's backed up elseqhere

--- old_doc/sphinx/UPLOAD.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-PRACTICAL=13
-HTML=AdKTutorial
-
-make html
-(cd .. && tar -jcvf AdKTutorial.tar.bz2 AdKTutorial/ )
-
-rsync -avP --delete _build/html/ becksteinlab:public_html/pages/courses/2013/SimBioNano/${PRACTICAL}/${HTML}
-cput $PRACTICAL ../AdKTutorial.tar.bz2 
