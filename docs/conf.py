f8f7a191168f7c9d88fc4489b0798cb68d8b3ba4
Sean Seyler <slseyler@asu.edu>
2017-03-28 17:22:23 -0700

    Added back mathjax and ifconfig extensions

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,8 @@ import shlex
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    # 'sphinx.ext.mathjax',
-    # 'sphinx.ext.ifconfig',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
